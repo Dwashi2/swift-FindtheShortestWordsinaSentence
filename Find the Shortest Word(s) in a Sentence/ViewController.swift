57d07348c308e7d0ace97678b22c3047380bb570
Daniel Washington <danielvertigo@hotmail.com>
2021-06-23 21:39:08 -0300

    Initial Commit

---
+++ Find the Shortest Word(s) in a Sentence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Find the Shortest Word(s) in a Sentence
+//
+//  Created by Daniel Washington Ignacio on 23/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
